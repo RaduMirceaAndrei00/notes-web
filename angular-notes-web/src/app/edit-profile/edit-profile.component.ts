9e7e11ff38c32c993977b499f1139b802928d26a
RaduMirceaAndrei00 <mirad28@yahoo.com>
2020-01-13 12:27:41 +0200

    Am adaugat o fereastra de editare a profilului

--- angular-notes-web/src/app/edit-profile/edit-profile.component.ts
+++ angular-notes-web/src/app/edit-profile/edit-profile.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, switchMap, take } from 'rxjs/operators';
+
+import { ProfileService } from '../profile.service';
+import { SessionService } from '../session.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +13,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
+  
+  public resultMessage = '';
+  public firstName = '';
+  public lastName = '';
+  public userName = '';
+  public phoneNumber = '';
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    userName: new FormControl(''),
+    phoneNumber: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(
+    private _sessionService: SessionService,
+    private _profile: ProfileService,
+    private _activatedRoute: ActivatedRoute, 
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this._sessionService.jwt$.pipe(switchMap(
+      token => this._profile.getProfile(token || '')
+    )).subscribe((userProfile: any) => {
+      this.firstName = userProfile.firstName;
+      this.lastName = userProfile.lastName;
+      this.userName = userProfile.userName;
+      this.phoneNumber = userProfile.phoneNumber;
+    });
+  }
+
+  async editProfile() {
+
+    try {
+
+      if( !this.profileForm.valid ) return;
+      await this._sessionService.jwt$.pipe(switchMap(
+        token => this._profile.editProfile(this.profileForm.value, token || ''))).toPromise();
+    } catch(error) {
+      this.resultMessage = error.payload.message;
+    }
   }
 
 }
--- angular-notes-web/src/app/profile.service.ts
+++ angular-notes-web/src/app/profile.service.ts
@@ -19,4 +19,23 @@ export class ProfileService {
     })
     .pipe(catchError(err => throwError(err.error)));
   }
+  getProfile (token: string) {
+    const url = `http://localhost:8084/user`;
+    return this._httpClient.get<any>(url, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .pipe(map((result:any) => result))
+    .pipe(catchError(err => throwError(err.error)));
+  }
+  editProfile (params: any, token: string) {
+    const url = `http://localhost:8084/users/editAccount`;
+    return this._httpClient.put(url, params, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .pipe(catchError(err => throwError(err.error)));
+  }
 }
